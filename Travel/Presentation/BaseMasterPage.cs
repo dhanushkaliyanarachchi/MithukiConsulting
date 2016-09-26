87bd49584a08f20c858b8a2d2c06ca81b77eb075
navaseelan <navaseelan@inexisconsulting.com>
2016-09-26 21:12:26 +0530

    1) Second commit

---
+++ Travel/Presentation/BaseMasterPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Travel
+{
+    public abstract class BaseMasterPage : System.Web.UI.MasterPage
+    {
+
+    }
+}
